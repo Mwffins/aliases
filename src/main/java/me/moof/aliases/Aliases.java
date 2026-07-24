979f10f52dab94ee454833a5464e9e45d8d98875
Moof <111189250+Mwffins@users.noreply.github.com>
2026-07-24 21:36:03 +0100

    initial commit

---
+++ src/main/java/me/moof/aliases/Aliases.java
@@ -0,0 +1,19 @@
+package me.moof.aliases;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Aliases implements ModInitializer {
+	public static final String MOD_ID = "aliases";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Loading command aliases.");
+	}
+}
